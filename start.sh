e1cbab341d2192160bca223eec9e14aac9a25d77
Gaétan Ramet <gr@visium.ch>
2020-05-08 16:27:24 +0200

    best model

---
+++ start.sh
@@ -0,0 +1,4 @@
+rm -rf data
+mkdir data
+wget https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/dibrd/v0.1/train.csv -O data/train.csv
+wget https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/dibrd/v0.1/test.csv -O data/test.csv
